17724841b39e4399e972fb695d3d94a62b0db0ef
washryan <washryan.3103@gmail.com>
2025-07-20 17:51:31 -0300

    Melhoria nos estilos e ajustes de layout
    - Adicionado efeito hover no botão primário com color.scale para evitar funções depreciadas do Sass
    - Ajustado padding lateral nas seções de evento para 5% (responsividade aprimorada)
    - Organização e importação dos módulos no main.scss

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,28 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
-const timeStampDoEvento = dataDoEvento.getTime();
+let dataDoEvento = new Date("Dec 12, 2025 19:00:00");
 
 const contaAsHoras = setInterval(function() {
     const agora = new Date();
     const timeStampDoAtual = agora.getTime();
+    const timeStampDoEvento = dataDoEvento.getTime();
 
-    const distanciaAteOEvento = timeStampDoEvento - timeStampDoAtual;
+    let distanciaAteOEvento = timeStampDoEvento - timeStampDoAtual;
 
     const diaEmMs = 1000 * 60 * 60 * 24;
     const HorasEmMs = 1000 * 60 * 60;
-    const MinutosEmMs =  1000 * 60;
+    const MinutosEmMs = 1000 * 60;
+
+    if (distanciaAteOEvento <= 0) {
+        dataDoEvento = new Date(agora.getTime() + (12 * diaEmMs));
+        distanciaAteOEvento = dataDoEvento.getTime() - timeStampDoAtual;
+    }
 
     const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
     const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / HorasEmMs);
     const minutosAteOEvento = Math.floor((distanciaAteOEvento % HorasEmMs) / MinutosEmMs);
     const segundosAteOEvento = Math.floor((distanciaAteOEvento % MinutosEmMs) / 1000);
 
-    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
-
-    if (distanciaAteOEvento < 0) {
-        clearInterval(contaAsHoras);
-        document.getElementById('contador').innerHTML = 'Evento Expirado'
-    }
-}, 1000)
+    document.getElementById('contador').innerHTML =
+        `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+}, 1000);
