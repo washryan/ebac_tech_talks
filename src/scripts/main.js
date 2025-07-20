2b2d3b6af4ef96cb9d48adbdd8d1cdd9d1a443f0
washryan <washryan.3103@gmail.com>
2025-07-20 17:38:03 -0300

    feat: adiciona contagem regressiva funcional ao projeto
    - Criação da lógica de contagem com setInterval e clearInterval
    - Uso de Date e getTime para manipular datas
    - Atualização dinâmica do contador na hero section
    - Adição do script de build no package.json

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampDoAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampDoAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const HorasEmMs = 1000 * 60 * 60;
+    const MinutosEmMs =  1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / HorasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % HorasEmMs) / MinutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % MinutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento Expirado'
+    }
+}, 1000)
