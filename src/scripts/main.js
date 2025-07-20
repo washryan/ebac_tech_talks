8325f0ee474361d70ba6407d55a52c37b45282dc
washryan <washryan.3103@gmail.com>
2025-07-20 18:24:23 -0300

    refactor: mudança no conteudo do site a pedido do professor para envio com o tema de aniversário. Não alterou nenhuma funcionalidade. - index.html: Mudança do tema; - hero: Mudança no fundo para tema novo; - infos_bar: mudança na coloração do txt para funcionalidade de mudança da data. - images/: reestruturação das imagens para tema novo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,42 @@
 AOS.init();
 
-let dataDoEvento = new Date("Dec 12, 2025 19:00:00");
+function getProximoAniversario() {
+    const hoje = new Date();
+    let ano = hoje.getFullYear();
+
+    const aniversario = new Date(`${ano}-03-31T20:00:00`);
+    if (hoje > aniversario) {
+        ano++;
+    }
+
+    return new Date(`${ano}-03-31T20:00:00`);
+}
+
+const dataDoEvento = getProximoAniversario();
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const spanQuando = document.getElementById('data-evento');
+spanQuando.innerText = `${dataDoEvento.toLocaleDateString('pt-BR')} às 20h`;
 
 const contaAsHoras = setInterval(function() {
     const agora = new Date();
     const timeStampDoAtual = agora.getTime();
-    const timeStampDoEvento = dataDoEvento.getTime();
 
-    let distanciaAteOEvento = timeStampDoEvento - timeStampDoAtual;
+    const distanciaAteOEvento = timeStampDoEvento - timeStampDoAtual;
 
     const diaEmMs = 1000 * 60 * 60 * 24;
-    const HorasEmMs = 1000 * 60 * 60;
-    const MinutosEmMs = 1000 * 60;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
 
-    if (distanciaAteOEvento <= 0) {
-        dataDoEvento = new Date(agora.getTime() + (12 * diaEmMs));
-        distanciaAteOEvento = dataDoEvento.getTime() - timeStampDoAtual;
+    const dias = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horas = Math.floor((distanciaAteOEvento % diaEmMs) / horasEmMs);
+    const minutos = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundos = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${dias}d ${horas}h ${minutos}m ${segundos}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'A festa começou!';
     }
-
-    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
-    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / HorasEmMs);
-    const minutosAteOEvento = Math.floor((distanciaAteOEvento % HorasEmMs) / MinutosEmMs);
-    const segundosAteOEvento = Math.floor((distanciaAteOEvento % MinutosEmMs) / 1000);
-
-    document.getElementById('contador').innerHTML =
-        `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
 }, 1000);
